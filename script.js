616b401a262aeb0a5fdd8d42d2e5fc576a3a4b60
Lonz8576 <Lonz8576@users.noreply.github.com>
2024-06-07 14:05:40 -0400

    Js for nav
    tested w/ console log to ensure T or F

---
+++ script.js
@@ -0,0 +1,16 @@
+let nav = document.querySelector(".primary-navigation");
+let navToggle = document.querySelector(".mobile-nav");
+
+
+navToggle.addEventListener("click", () => {
+
+    let visiblity = nav.getAttribute("data-visible");
+    if (visiblity === "false") {
+        nav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+    } else {
+        nav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+    }
+     console.log(navToggle.getAttribute('aria-expanded'));
+})
